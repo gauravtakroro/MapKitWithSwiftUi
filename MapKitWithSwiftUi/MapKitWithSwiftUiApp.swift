fb485ddbbbbc3c992d26eb35c97d1d55e0ea0018
Gaurav Tak <gaurav.tak@roro.io>
2023-09-10 22:24:30 +0530

    Initial Commit

---
+++ MapKitWithSwiftUi/MapKitWithSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitWithSwiftUiApp.swift
+//  MapKitWithSwiftUi
+//
+//  Created by Roro Solutions LLP on 10/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MapKitWithSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
