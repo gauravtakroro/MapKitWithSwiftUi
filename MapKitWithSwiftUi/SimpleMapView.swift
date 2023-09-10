d4fcde18e1e74881833bfdbb3706fdbef6b44e3c
Gaurav Tak <gaurav.tak@roro.io>
2023-09-11 01:18:42 +0530

    SimpleMapView implemented

--- MapKitWithSwiftUi/SimpleMapView.swift
+++ MapKitWithSwiftUi/SimpleMapView.swift
@@ -11,7 +11,35 @@ import CoreLocation
 
 struct SimpleMapView: View {
     var body: some View {
-        Text("Simple Map View")
+        MapView()
+    }
+}
+
+struct MapView: UIViewRepresentable {
+    // Location Manager object to request location updates from the core API
+    var locationManager = CLLocationManager()
+    // Setup function for the representable
+    func setup() {
+        // Set the location manager properties
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestAlwaysAuthorization()
+    }
+    // Function to configure the representable view
+    func makeUIView(context: Context) -> MKMapView {
+        // Call the setup function
+        setup()
+        // Initialize the representable view
+        let mapView = MKMapView(frame: UIScreen.main.bounds)
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        
+        // Return the representable view
+        return mapView
+    }
+    // Function to update the representable view
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+      // Do nothing
     }
 }
 
